8a1ce3b3b8f604dcf583122dea7dbe59773f1704
zakiroc <164227048+zakiroc@users.noreply.github.com>
2024-06-26 21:24:14 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+
+
+let n = +prompt(' 1-7:hafta kunini kiriting')
+
+switch (n) {
+    case 1:
+        console.log('понедельник');
+        
+        break;
+        case 2:
+        alert('вторник');
+        
+        break;
+        case 3:
+        alert('среда');
+        
+        break;
+        case 4:
+        alert('четверг');
+        
+        break;
+        case 5:
+       alert('пятница');
+        
+        break;
+        case 6:
+        alert('суббота');
+        
+        break;
+        case 7:
+        alert('воскресенье');
+        
+        
+
+       
+
+    default:
+        alert('hafta kunini kiritishni soraymiz');
+        break;
+}
